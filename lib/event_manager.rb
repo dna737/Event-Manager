b97be11da1aaf5b06c22cafa7bb055011a7ef76b
dna737 <adnanhere27@gmail.com>
2023-01-09 15:43:37 +0530

    fetch legislators using civicinfo api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,20 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 puts "Event Manager initialized!"
 
-file_path = "/home/adnan/ruby/projects/event_manager/event_attendees.csv"
+file_path = "/home/adnan/ruby/projects/event_manager/Event-Manager/event_attendees.csv"
 if File.exist?(file_path)
   contents = CSV.open(
     file_path, 
     headers: true,
     header_converters: :symbol
     )
+else 
+  puts "File does not exist"
 end
 
 def clean_zipcode(zipcode)
@@ -20,5 +26,12 @@ contents.each do|row|
 
   zipcode = clean_zipcode(row[:zipcode])
   
-  puts "#{name} #{zipcode}"
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
